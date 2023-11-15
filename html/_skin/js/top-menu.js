fee15dae36987ce63a5ed1f6129d37c3e079bc79
surean-workspace <surean.mir9@gmail.com>
2023-11-16 05:59:37 +0900

    Add: header 스타일

---
+++ html/_skin/js/top-menu.js
@@ -0,0 +1,159 @@
+$(function () {
+	// menu button
+	var num = 0;
+	var flag = true;
+	$('.m-menu-btn')
+		.unbind('click')
+		.bind('click', function () {
+			num++;
+			if (flag) {
+				$('#header .m-gnb-bg').fadeIn();
+				$('#header .gnb').animate({ right: 0, opacity: 1 }, 500);
+				$('#header .m-lang').animate({ right: 0, opacity: 1 }, 500);
+				//			$("body").css("overflow","hidden");
+				$('body').addClass('gnb-open');
+				flag = false;
+			} else {
+				$('#header .m-gnb-bg').fadeOut();
+				$('#header .gnb').animate({ right: -100 + '%', opacity: 1 }, 500);
+				$('#header .m-lang').animate({ right: -100 + '%', opacity: 0 }, 500);
+				flag = true;
+				//			$("body").css("overflow","visible");
+				$('body').removeClass('gnb-open');
+			}
+			$(this).toggleClass('on');
+		});
+
+	// gnb current menu
+	var _pathname = location.pathname;
+	_pathname = _pathname.split('/');
+	if ($('html').attr('lang') == 'ko') {
+		_pathname = _pathname[1];
+	} else {
+		_pathname = _pathname[2];
+	}
+	$('#header .gnb > ul > li > a').each(function () {
+		var _href = $(this).attr('href');
+		if (_href.indexOf(_pathname) != -1) $(this).addClass('current');
+	});
+
+	// mobile 2depth
+	function gnbClickOn() {
+		$('.gnb>ul>li>a')
+			.unbind('click')
+			.bind('click', function (e) {
+				var tag = $(this).parent('li').find('li');
+				if (tag.length != 0) {
+					e.preventDefault();
+					$('.gnb>ul>li>a').not(this).parent().removeClass('on').children('ul').slideUp(200);
+					$(this)
+						.parent()
+						.addClass('on')
+						.children('ul')
+						.slideToggle(200, function () {
+							if ($(this).css('display') == 'none') {
+								$(this).parent().removeClass('on');
+							}
+						});
+				}
+			});
+	}
+	function gnbClickOff() {
+		$('.gnb>ul>li>a')
+			.unbind('click')
+			.bind('click', function (e) {
+				e.stopPropagation();
+			});
+	}
+	$(document).ready(function () {
+		if (matchMedia('screen and (max-width: 1199px)').matches) {
+			gnbClickOn();
+		} else {
+			gnbClickOff();
+		}
+
+		$('#searchBtn').on('click', function (e) {
+			e.preventDefault();
+			$('.search_box').toggleClass('block');
+		});
+	});
+	$(window).on('load resize', function () {
+		if (matchMedia('screen and (max-width: 1199px)').matches) {
+			gnbClickOn();
+		} else {
+			gnbClickOff();
+		}
+	});
+
+	// var sta = 50;
+	$(window).scroll(function () {
+		var w = $(window).width() + 18;
+		var sta = $('.sta').outerHeight();
+		if ($(window).scrollTop() >= sta) {
+			$('#header').addClass('fixed');
+		} else {
+			$('#header').removeClass('fixed');
+		}
+	});
+	$(window).on('load resize', function () {
+		var w = $(window).width() + 18;
+		var sta = $('.sta').outerHeight();
+		if ($(window).scrollTop() >= sta) {
+			$('#header').addClass('fixed');
+		} else {
+			$('#header').removeClass('fixed');
+		}
+	});
+
+	/* sitemap */
+	var headerSitemap = $('#headerSitemap');
+	var gnbList = $('#header .gnb > ul');
+	gnbList.clone().appendTo(headerSitemap);
+
+	var sitemapBtn = $('.pc_sitemap_btn');
+	sitemapBtn.on('click', function () {
+		$(this).toggleClass('on');
+		headerSitemap.toggleClass('on');
+	});
+
+	/* footer family site */
+	$(function () {
+		$('.familySite h3 a').on('click', function (e) {
+			e.preventDefault();
+			if ($('.familySite').hasClass('on')) {
+				$('.familySite').removeClass('on');
+				$('.footSelect > ul').slideUp();
+			} else {
+				$('.familySite').addClass('on');
+				$('.footSelect > ul').slideDown();
+			}
+		});
+		$(document).click(function (event) {
+			if (!$(event.target).closest('.familySite').length) {
+				$('.familySite').removeClass('on');
+				$('.footSelect > ul').slideUp();
+			}
+		});
+	});
+
+	// 전자족보
+	if ($('.header__sta .sta__btn-genealogy').length === 1) {
+		var btn = $('.header__sta .sta__btn-genealogy');
+		var cloneBtn = btn.clone();
+    var breakpoint = 576;
+
+		if ($(window).width() < breakpoint) {
+			cloneBtn.prependTo('#header .sta > .inner');
+		} else {
+			cloneBtn.remove();
+		}
+
+		$(window).on('resize', function () {
+			if ($(window).width() < breakpoint) {
+				cloneBtn.prependTo('#header .sta > .inner');
+			} else {
+				cloneBtn.remove();
+			}
+		});
+	}
+});
---
+++ html/_skin/layout/inc_top_menu.php
@@ -0,0 +1,85 @@
+<?php
+$locale = getLocale();
+$arrLocalType = getCFG("LocaleConfig");
+$_SERVER['REQUEST_URI'] = RemoveXSS(preg_replace('/\/'.$locale.'/','',$_SERVER['REQUEST_URI']));
+
+$arrCategory = array();
+// 카테고리별 depth 구하기
+if (!empty($category_code)) {
+    for($i=2; $i<=strlen($category_code); $i+=2) {
+        $arrCategory[$i/2] = substr(@$category_code,0,$i);
+    }
+}
+?>
+
+<!-- header -->
+<header id="header">
+	<div class="sta">
+		<div class="inner lang-depth1">
+			<?php include('html/_skin/'._SKIN.'/layout/inc_social_link.php');?>
+
+			<ul>
+				<!-- <li class="line-r mb_none">
+					<a href="/">Home</a>
+				</li> -->
+<?php
+				if (!getLoginId()) {	// 로그인 전
+?>
+					<li class="type-member line-r"><a href="/member/login"><?=_LANG('로그인')?></a></li>
+					<li class="type-member"><a href="/member/stipulation"><?=_LANG('회원가입')?></a></li>
+<?php
+			   }
+				else {					// 로그인 후
+					if(getLoginLevel()!="99"){  // 회원
+?>
+					<li class="type-member line-r"><a href="/member/form-edit"><?=_LANG('마이페이지')?></a></li>
+<?php
+					}
+?>
+					<li><a href="/index.php?tpf=_module/member/logout"><?=_LANG('로그아웃')?></a></li>
+<?php
+				}
+?>
+				<!-- <li class="lang-depth1-li">
+					<a href="#">Other languages</a>
+					<ul class="lang-depth2">
+						<?php
+						if (!empty($arrLocalType)) {
+							foreach($arrLocalType['LocaleType'] as $key => $val) {
+                                // echo '<li class="lang"><a href="/index.html?locale='.$key.'&return_url='.urlencode($_SERVER['REQUEST_URI']).'">'.$val.'</a></li>';
+                                echo '<li class="lang"><a href="/index.html?locale='.$key.'&return_url=/main">'.$val.'</a></li>';
+							}
+						}
+						?>
+					</ul>
+				</li> -->
+			</ul>
+		</div>
+	</div>
+	<div class="header-bottom">
+		<p class="logo"><a href="/"><img src="/html/_skin/img/common/logo.png" alt="logo"></a></p>
+		<div class="m-menu-btn">
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
+		<nav class="gnb">
+			<div class="m-lang">
+
+			</div>
+      <?php include('html/_skin/'._SKIN.'/layout/inc_navigation.php');?>
+			<div class="m-gnb-bg"></div>
+		</nav>
+    <div class="header__sta">
+      <a href="/electronic-genealogy" class="sta__btn-genealogy"><?=_LANG('전자족보')?></a>
+    </div>
+		<div class="pc_sitemap_btn">
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
+	</div>
+	<div id="headerSitemap"></div>
+</header>
+<!-- //header -->
+<script type="text/javascript" src="/html/_skin/<?=_SKIN?>/js/top-menu.js"></script>
